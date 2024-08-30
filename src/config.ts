ffe8fdd5c17b045955e6835aebea0255a0681355
tinproht123 <tinproht123@gmail.com>
2024-08-30 15:32:32 +0700

    update

--- src/config.ts
+++ src/config.ts
@@ -7,16 +7,16 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
+  title: 'Blog Amigos v2',
+  subtitle: 'Blog Amigos',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    enable: true,
+    src: './assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
     credit: {
       enable: false,         // Display the credit text of the banner image
@@ -47,9 +47,9 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/power-csm.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Khanh Tung',
+  bio: 'A Web Devloper, and a lazy blogger',
   links: [
     {
       name: 'Twitter',
--- src/i18n/i18nKey.ts
+++ src/i18n/i18nKey.ts
@@ -3,6 +3,8 @@ enum I18nKey {
   about = 'about',
   archive = 'archive',
   search = 'search',
+  login = 'login',
+  logout = 'logout',
 
   tags = 'tags',
   categories = 'categories',
